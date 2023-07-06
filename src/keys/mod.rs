136003920dff1d8e40d9a2e9912d79902ffedc30
lthiery <thiery.louis@gmail.com>
2023-07-06 10:11:42 -0700

    implement message creation with signing and roundtrip test

--- src/keys/mod.rs
+++ src/keys/mod.rs
@@ -5,7 +5,7 @@ use std::result::Result;
 pub mod file;
 
 pub trait KeyTrait {
-    type Error: core::fmt::Debug;
+    type Error: core::fmt::Debug + core::fmt::Display;
     fn pubkey(&self) -> Result<PublicKey, Self::Error>;
     fn sign(&self, msg: &[u8]) -> Result<Vec<u8>, Self::Error>;
 }
